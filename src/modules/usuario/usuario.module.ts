c9c68ae73c6e8674cf5d74175448de22d69997d3
lucas marques <marqueslukas66@gmail.com>
2022-08-17 11:11:38 -0300

    feat(usuario.module): Esconder url do banco de dados

--- src/modules/usuario/usuario.module.ts
+++ src/modules/usuario/usuario.module.ts
@@ -7,14 +7,11 @@ import { collections } from '../../shared/tabelas/collections';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:senha@cluster0.aohxg.mongodb.net/organizado',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        connectionName: Collection.connectionName,
-      },
-    ),
+    MongooseModule.forRoot(process.env.DATABASE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectionName: Collection.connectionName,
+    }),
     MongooseModule.forFeature(collections, Collection.connectionName),
   ],
   providers: [UsuarioService],
