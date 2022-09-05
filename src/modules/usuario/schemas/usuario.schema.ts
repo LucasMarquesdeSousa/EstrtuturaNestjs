b08b5789b5e9dd3fa4e7dcb60ae8181eb2331ad3
lucas marques <marqueslukas66@gmail.com>
2022-09-05 15:58:59 -0300

    feat(usuarioSchemas): Alteracao da criacao dos schemas do mongoDB

--- src/modules/usuario/schemas/usuario.schema.ts
+++ src/modules/usuario/schemas/usuario.schema.ts
@@ -1,11 +1,27 @@
 import * as mongoose from 'mongoose';
 import { collectionsName } from '../../../shared/tabelas/collectionsName';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
-export const UsuarioSchema = new mongoose.Schema(
-  {
-    nome: { type: String, unique: false },
-    email: { type: String, unique: true },
-    senha: { type: String, unique: false },
-  },
-  { timestamps: true, collection: collectionsName.USUARIO },
-);
+export type IUsuario = Usuario & Document;
+
+@Schema()
+export class Usuario {
+  @Prop()
+  nome: string;
+
+  @Prop()
+  senha: string;
+
+  @Prop()
+  email: string;
+}
+
+export const UsuarioSchema = SchemaFactory.createForClass(Usuario);
+// export const UsuarioSchema = new mongoose.Schema(
+//   {
+//     nome: { type: String, unique: false },
+//     email: { type: String, unique: true },
+//     senha: { type: String, unique: false },
+//   },
+//   { timestamps: true, collection: collectionsName.USUARIO },
+// );
--- src/modules/usuario/service/Usuario.service.ts
+++ src/modules/usuario/service/Usuario.service.ts
@@ -2,15 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UsuarioInterface } from '../Interface/usuario.interface';
-import {collectionsName} from "../../../shared/tabelas/collectionsName";
-import {collections} from "../../../shared/tabelas/collections";
-import {Collection} from "../../../shared/tabelas/collection";
+import { collectionsName } from '../../../shared/tabelas/collectionsName';
+import { collections } from '../../../shared/tabelas/collections';
+import { Collection } from '../../../shared/tabelas/collection';
+import { IUsuario } from '../schemas/usuario.schema';
 
 @Injectable({})
 export class UsuarioService {
   constructor(
     @InjectModel(collectionsName.USUARIO, Collection.connectionName)
-    private usuarioModel: Model<UsuarioInterface>,
+    private usuarioModel: Model<IUsuario>,
   ) {}
 
   async findOne(email: string): Promise<UsuarioInterface | undefined> {
